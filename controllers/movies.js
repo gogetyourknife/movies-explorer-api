f3394f33e8e73f77a08a41af8863d1941df44f2b
Luna Hogart <moontochka@gmail.com>
2022-10-23 12:15:01 +0500

    fix: getmovies function, ID filter added

--- controllers/movies.js
+++ controllers/movies.js
@@ -11,7 +11,7 @@ const {
 } = require('../utils/errors/error-names');
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
+  Movie.find({ owner: req.user._id })
     .then((movie) => {
       res.send(movie);
     })
